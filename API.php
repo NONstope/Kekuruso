f37bc5764fbe9cbc9e1ec9476f30141a74ac966f
renair <platinium9889@gmail.com>
2015-11-20 23:42:29 +0200

    add some new functions to chatAPI(login(),register(),is_logined()) and salt

--- API.php
+++ API.php
@@ -1,6 +1,7 @@
 <?php
 
 $MYSQL_HANDLE = "";
+$SALT = "sm_salt";
 
 /*
  * set connection to base
@@ -16,14 +17,56 @@ function set_connection($host,$user,$pass,$bd_name)
  */
 function is_user_exist($login, $password)
 {
-	global $MYSQL_HANDLE;
-	$answer = mysqli_query($MYSQL_HANDLE, "select * from `users` where `login`='".$login."' and `password`='".$password."';");
+	global $MYSQL_HANDLE, $SALT;
+	$answer = mysqli_query($MYSQL_HANDLE, "select * from `users` where `login`='$login' and `password`='".md5($password.$SALT)."';");
 	$arr = mysqli_fetch_array($answer);
 	if(count($arr) == 8 && count(mysqli_fetch_array($answer)) == 0)
+	{
+		return $arr;
+	}
+	return false;
+}
+
+
+function login($login, $password)
+{
+	global $MYSQL_HANDLE, $SALT;
+	if(($arr = is_user_exist($login, $password)) != false)
+	{
+		setcookie("login",$arr['login']);
+		setcookie("id",$arr['id']);
+		setcookie("nick",$arr['nickname']);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function register($login, $password, $nick)
+{
+	global $MYSQL_HANDLE, $SALT;
+	$password = md5($password.$SALT);
+	if(is_user_exist($login, $password) == false)
+	{
+		mysqli_query($MYSQL_HANDLE, "insert into `users` set `login`='$login', `password`='$password', `nickname`='$nick';");
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+function is_logined()
+{
+	if(isset($_COOKIE['id']) && isset($_COOKIE['login']) && isset($_COOKIE['nick']))
 	{
 		return true;
 	}
 	return false;
 }
+
 
 ?>
--- index.php
+++ index.php
@@ -1,25 +1,29 @@
+<?php 
+include 'API.php';
+set_connection("localhost", "root", "", "kekuruso");
+if(isset($_POST['login']) && isset($_POST['password']) && login($_POST['login'], $_POST['password']) !== false)
+{
+	//
+}
+if(is_logined())
+{
+	echo "Logined!";
+}
+?>
 <html>
 	<head>
 		<title>progect</title>
-		
 	</head>
 	<body >
 		<form action="" method="post" class= "form1">
 			Login: <input type="text" name="login">
 			Password: <input type="text" name="password">
+			Nickname: <input type="text" name="nick">
 			<input type="submit">
 		</form>
 		<?php 
-		include 'API.php';
-		set_connection("localhost", "root", "", "kekuruso");
-		if(is_user_exist($_POST['login'], $_POST['password']))
-		{
-			echo 'Succed!';
-		}
-		else
-		{
-			echo "<br>Fail(<br>";
-		}
+		//$text = file_get_contents("login.ext");
+		//echo($text);
 		?>
 	</body>
 </html>
