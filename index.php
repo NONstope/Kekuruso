dd9809bf9db6d15f74b220423cf619294cb99c8b
renair <platinium9889@gmail.com>
2015-11-23 01:01:14 +0200

    so-so version

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <?php 
-include 'API.php';
-set_connection("localhost", "root", "", "kekuruso");
-if(isset($_POST['login']) && isset($_POST['password']) && login($_POST['login'], $_POST['password']) !== false)
-{
-	//
-}
-if(is_logined())
-{
-	echo "Logined!";
-}
-?>
+	include 'API.php';
+	set_connection("localhost", "root", "", "kekuruso");
+	
+	$logi = htmlspecialchars($_POST['login'] );
+	$pas = htmlspecialchars($_POST['password']); 
+	
+	if(is_user_exist($logi, $pas) == false){
+		header("Location: reg.html");
+	}
+	else{
+		login($logi, $pas);
+		header("Location: action3.php");
+	}
+ ?>
+
 <html>
 	<head>
-		<title>progect</title>
+		<title>login</title>
+		<link rel="stylesheet" href="login.css">
 	</head>
-	<body >
-		<form action="" method="post" class= "form1">
-			Login: <input type="text" name="login">
-			Password: <input type="text" name="password">
-			Nickname: <input type="text" name="nick">
-			<input type="submit">
+	<body>
+	<div class ="div_a">
+		<a href ="login.html" class = "a1">log in</a>
+		<a href ="reg.html" class = "a2">registration</a>
+	</div>
+	<div class= "formachka">
+		<form action="" method="post">
+			<p class ="ppp"> login: <br> <input type="text" name="login" /></p>
+			<p class ="ppp"> password: <br> <input type="text" name="password" /></p>
+			<p class ="pp"><input type="submit" value="Login in" class = "mysubmit"/></p>
 		</form>
-		<?php 
-		//$text = file_get_contents("login.ext");
-		//echo($text);
-		?>
+	</div>
 	</body>
 </html>
